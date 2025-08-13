0dd44fff4b5c1afafcc57f29ea9aa620a10c65c3
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-13 14:21:33 +0800

    Add files via upload

---
+++ remix.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  appDirectory: "app",
+  serverBuildTarget: "netlify",
+  serverModuleFormat: "cjs",
+  ignoredRouteFiles: ["**/.*"]
+};
