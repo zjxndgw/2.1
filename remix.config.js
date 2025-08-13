4e8eb4582ff0f73ca1c5550b39559bf55c94ed43
zjxndgw <97914540+zjxndgw@users.noreply.github.com>
2025-08-13 14:45:57 +0800

    Update and rename remix.config.js to README.md

--- remix.config.js
+++
@@ -1,9 +0,0 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
-module.exports = {
-  appDirectory: "app",
-  serverBuildTarget: "netlify",
-  serverModuleFormat: "cjs",
-  ignoredRouteFiles: ["**/.*"]
-};
